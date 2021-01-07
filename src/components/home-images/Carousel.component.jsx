ddae37568c0f16120398ecfab4df78381d97dd1d
Ajibola Lawal <ajisofteng@gmail.com>
2021-01-07 20:07:56 +0000

    update commit

--- src/components/home-images/Carousel.component.jsx
+++ src/components/home-images/Carousel.component.jsx
@@ -4,17 +4,19 @@ import Carousel from 'react-bootstrap/Carousel'
 
 import Warriors from "../../assets/HomeImages/Warriors.jpg";
 import Warriors2 from "../../assets/HomeImages/Warriors2.JPG";
-import Madrid from "../../assets/HomeImages/Madrid.jpg"
-import football2 from "../../assets/HomeImages/eric-reid.jpg";
-import football1 from "../../assets/HomeImages/football-photo.jpg";
+import Madrid from "../../assets/HomeImages/Madrid.jpg";
+import Test from "../../assets/HomeImages/Test.jpg"
 import football3 from "../../assets/HomeImages/football3.jpg";
+import Chess from "../../assets/HomeImages/Chess.jpg"
 
 const CarouselContainer = () => {
     const photos = [
         {src:Warriors, caption:"European Game"},
+        // f1
         {src:Madrid, caption:"With amigos in Madrid"},
-        {src:Warriors2, caption:"In the zone"},
-        {src:football3, caption:"Where I feel most at home!"}
+        {src:Test, caption:"Against The Blitz"},
+        {src:Chess, caption:"Sibling rivalry"},
+        {src:Warriors2, caption:"A friendly hug"},
     ];
     return(
         <Carousel>
@@ -23,9 +25,9 @@ const CarouselContainer = () => {
                 key={photo.src}
                 >
                     <img className="d-block images" src={photo.src} alt="home"/>
-                    <div className="caption-box">
+                    {/* <div className="caption-box">
                         <h3>{photo.caption}</h3>
-                    </div>
+                    </div> */}
                 </Carousel.Item>
             ))}
         </Carousel>
--- src/components/pages/Portfolio/ProjectList.jsx
+++ src/components/pages/Portfolio/ProjectList.jsx
@@ -10,9 +10,9 @@ const projectList = [
     { description: "Mock E-commerce shop which accepts Card payments through Stripe API.", name: "E-Commerce Shop", thumbnail: eCommerce, visible: true, link: "https://red-sea-clothing.herokuapp.com/", tech: ["ALL", "REACT", "FIREBASE"] },
     { description: "Movie comparison site where users can also rate and store their favourite movie.s", name: "Movie Review App", thumbnail: movieSandpit, visible: true, link: "https://serene-beyond-29185.herokuapp.com/", tech: ["ALL", "JQUERY", "BOOTSTRAP", "NODE JS", "JAVASCRIPT", "MONGODB"] },
     { description: "Campground review websites where users can add visited campgrounds and comment on others campground reviews.", name: "Yelp Camp Site", thumbnail: YelpCamp, visible: true, link: "https://aqueous-temple-44263.herokuapp.com/", tech: ["ALL", "HTML/CSS", "JQUERY", "NODE JS", "JAVASCRIPT", "MONGODB"] },
-    { description: "Player reacts to five lights which is followed by onboard video of real F1 car starting a race.", name: "F1 Reaction Game", thumbnail: reactionTestGame, visible: true, link: "https://guarded-depths-78983.herokuapp.com/", tech: ["ALL", "HTML/CSS", "JQUERY", "JAVASCRIPT"] },
+    // { description: "Player reacts to five lights which is followed by onboard video of real F1 car starting a race.", name: "F1 Reaction Game", thumbnail: reactionTestGame, visible: true, link: "https://guarded-depths-78983.herokuapp.com/", tech: ["ALL", "HTML/CSS", "JQUERY", "JAVASCRIPT"] },
     { description: "Remake of the popular yahtzee game.", name: "Yahtzee Game", thumbnail: yahtzee, visible: true, link: "https://yahtzee-game-app.herokuapp.com/", tech: ["ALL", "REACT", "BOOTSTRAP"] },
-    { description: "Porfolio website displaying my education, experiences and background.", name: "Portfolio Site", thumbnail: portfolioSite, visible: true, link: "", tech: ["ALL", "REACT", "BOOTSTRAP"] }
+    { description: "Porfolio website displaying my education, experiences and background.", name: "Portfolio Site", thumbnail: portfolioSite, visible: true, link: "/", tech: ["ALL", "REACT", "BOOTSTRAP"] }
 ];
 
 export default projectList;
